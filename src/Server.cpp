24ba7e46a1f678280486061a59dedf92ff056cec
ClémentMNT <clement.montoya@epitech.eu>
2024-04-04 11:16:56 +0200

    handle multiple client with event loop

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,101 +10,98 @@
 #include <vector>
 #include <algorithm>
 
-void handle_client(int client_fd) {
-  char pong[] = "+PONG\r\n";
-  while (true) {
-    char buf[1024];
-    int rc = recv(client_fd, &buf, sizeof(buf), 0);
-    if (rc <= 0) {
-      close(client_fd);
-      break;
-    }
-    send(client_fd, pong, strlen(pong), 0);
-  }
+int handle_client(int client_fd) {
+	char pong[] = "+PONG\r\n";
+	char buf[1024];
+	int rc = recv(client_fd, &buf, sizeof(buf), 0);
+	if (rc <= 0) {
+		close(client_fd);
+		return 84;
+	} else {
+		send(client_fd, pong, strlen(pong), 0);
+		return 0;
+	}
 }
 
 
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
-  
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
+	// Uncomment this block to pass the first stage
+	
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+	std::cerr << "Failed to create server socket\n";
+	return 1;
+	}
+	
+	// Since the tester restarts your program quite often, setting SO_REUSEADDR
+	// ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+		std::cerr << "setsockopt failed\n";
+		return 1;
+	}
+	
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(6379);
+	
+	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+		std::cerr << "Failed to bind to port 6379\n";
+		return 1;
+	}
+	
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
+		std::cerr << "listen failed\n";
+		return 1;
+	}
+	
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	
+	std::cout << "Waiting for a client to connect...\n";
 
-  fd_set master_set;
-  fd_set read_fds;
-  int max_fd = server_fd;
+	fd_set master_set;
+	fd_set read_fds;
+	int max_fd = server_fd;
 
-  FD_ZERO(&master_set);
-  FD_SET(server_fd, &master_set);
+	FD_ZERO(&master_set);
+	FD_SET(server_fd, &master_set);
 
-  while(true) {
-    read_fds = master_set;
-    if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
-      std::cerr << "select failed\n";
-      return 1;
-    }
+	while(true) {
+		read_fds = master_set;
+		if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
+			std::cerr << "select failed\n";
+			return 1;
+		}
 
-    for (int i = 0; i <= max_fd; i++) {
-      if (FD_ISSET(i, &read_fds)) {
-        if (i == server_fd) {
-          int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-          if (client_fd == -1) {
-            std::cerr << "accept failed\n";
-          } else {
-            FD_SET(client_fd, &master_set);
-            max_fd = std::max(max_fd, client_fd);
-            std::cout << "Client connected\n";
-          }
-        } else {
-          handle_client(i);
-          close(i);
-          FD_CLR(i, &master_set);
-        }
-      }
-    }
-  }
-  
-  // int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  // std::cout << "Client connected\n";
+		for (int i = 0; i <= max_fd; i++) {
+			if (FD_ISSET(i, &read_fds)) {
+				if (i == server_fd) {
+					int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+					if (client_fd == -1) {
+						std::cerr << "accept failed\n";
+					} else {
+						FD_SET(client_fd, &master_set);
+						max_fd = std::max(max_fd, client_fd);
+						std::cout << "Client connected\n";
+					}
+				} else {
+					if (handle_client(i) == 84) {
+						FD_CLR(i, &master_set);
+						close(i);
+						std::cout << "Client disconnected\n";
+					}
+				}
+			}
+		}
+	}
 
-  // handle_client(client_fd);
+	close(server_fd);
 
-  close(server_fd);
-
-  return 0;
+	return 0;
 }
